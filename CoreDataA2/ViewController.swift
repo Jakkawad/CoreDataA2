3e8018f188dc0d248f50f811d6a85c4f09279048
Jakkawad <jakkawadchaiplee@Jakkawads-MacBook-Pro.local>
2016-08-24 16:56:43 +0700

    Final
    Final + Code

--- CoreDataA2/ViewController.swift
+++ CoreDataA2/ViewController.swift
@@ -7,9 +7,87 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView:UITableView!
+    
+    var listItems = [NSManagedObject]()
+    
+    
+    @IBAction func addItem(sender: AnyObject) {
+        let alertController = UIAlertController(title: "Type", message: "Type...", preferredStyle: .Alert)
+        let confirmAction = UIAlertAction(title: "Confirm", style: UIAlertActionStyle.Default, handler: ({
+            (_) in
+            if let field = alertController.textFields![0] as? UITextField {
+                self.saveItem(field.text!)
+                self.tableView.reloadData()
+            }
+        }))
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        alertController.addTextFieldWithConfigurationHandler({
+            (textField) in
+            textField.placeholder = "Type in something!!!"
+        })
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func saveItem(itemToSave:String) {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let entity = NSEntityDescription.entityForName("ListEntity", inManagedObjectContext: managedContext)
+        let item = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        item.setValue(itemToSave, forKey: "item")
+        do {
+            try managedContext.save()
+            listItems.append(item)
+        }
+        catch {
+            print("Error")
+        }
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listItems.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell0 = tableView.dequeueReusableCellWithIdentifier("tableCell0")
+        let item = listItems[indexPath.row]
+        cell0?.textLabel?.text = item.valueForKey("item") as! String
+        return cell0!
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Right)
+        managedContext.deleteObject(listItems[indexPath.row])
+        listItems.removeAtIndex(indexPath.row)
+        self.tableView.reloadData()
+    }
+    override func viewWillAppear(animated: Bool) {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName: "ListEntity")
+        do {
+            let results = try managedContext.executeFetchRequest(fetchRequest)
+            listItems = results as! [NSManagedObject]
+        }
+        catch {
+            print("Error")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
